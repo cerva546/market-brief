ae0218f5977bbe2604e09da548111b5ace1045e6
cerva546 <cerveraan@uchicago.edu>
2026-03-24 19:54:27 -0500

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,13 +1,10 @@
 {
   "functions": {
-    "api/brief.js": { "maxDuration": 60 },
-    "api/quotes.js": { "maxDuration": 30 },
-    "api/cron.js": { "maxDuration": 60 }
-  },
-  "crons": [
-    {
-      "path": "/api/cron",
-      "schedule": "0 13 * * 1-5"
+    "api/brief.js": {
+      "maxDuration": 60
+    },
+    "api/quotes.js": {
+      "maxDuration": 30
     }
-  ]
+  }
 }
